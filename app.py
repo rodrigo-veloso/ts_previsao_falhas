1c978b9db92236bbdbbfdf239c756f7a01869859
Rodrigo <rodrigoveloso@id.uff.br>
2021-03-10 01:28:17 -0300

    10 commit

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import adfuller
+
+def calc_metrics(y,y_pred):
+  mse_naive = np.mean((y - y_pred)**2)
+  rmse_naive = np.mean((y - y_pred)**2)
+  mae = np.mean(np.abs(y - y_pred))
+
+  return mse_naive, rmse_naive, mae
+
 
 st.write("""# Séries Temporais Para Previsão de Falhas""")
 
@@ -32,3 +42,69 @@ ts = df[df['Equipamento'] == equipamento].groupby('Data').count().reset_index().
 ts = ts.rename(columns={'Classe':'Falhas'})
 st.line_chart(ts.rename(columns={'Data':'index'}).set_index('index'))
 st.write('Mínimo de falhas por mês: {}'.format(ts.min()['Falhas']))
+
+st.write("""## Modelo Naive""")
+
+ts = ts.set_index(['Data'])
+
+naive_model = ts.shift().dropna()
+
+metrics = calc_metrics(ts['Falhas'].values[1:],ts.shift().dropna()['Falhas'].values)
+
+st.write("""### MSE = {}""".format(metrics[0]))
+st.write("""### RMSE = {}""".format(metrics[1]))
+st.write("""### MAE = {}""".format(metrics[2]))
+
+
+plt.plot(naive_model,label='Naive')
+plt.plot(ts,label='Dados')
+#plt.legend()
+plt.ylabel('Falhas')
+plt.xlabel('Data')
+plt.legend()
+st.pyplot(plt)
+
+st.write("""## Teste de Dickey-Fuller Aumentado""")
+
+dftest = adfuller(ts['Falhas'],autolag='AIC')
+
+dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-Value','Lags Used','Observations'])
+resultado = 'estacionária' if dfoutput[1] < 0.05 else 'não estacionária'
+st.write(dfoutput)
+st.write('### A série é {}'.format(resultado))
+
+st.write("""## Modelo ARIMA""")
+
+from statsmodels.tsa.arima.model import ARIMA
+
+p = st.slider('P', min_value=0, max_value=10)
+d = st.slider('d', min_value=0, max_value=10)
+q = st.slider('Q', min_value=0, max_value=10)
+
+model = ARIMA(ts,order=(p,d,q))
+results_AR = model.fit()
+
+metrics = calc_metrics(ts['Falhas'].values,results_AR.fittedvalues.values)
+
+st.write("""### MSE = {}""".format(metrics[0]))
+st.write("""### RMSE = {}""".format(metrics[1]))
+st.write("""### MAE = {}""".format(metrics[2]))
+
+plt.clf()
+plt.plot(results_AR.fittedvalues,label='ARIMA')
+plt.plot(ts,label='Dados')
+#plt.legend()
+plt.ylabel('Falhas')
+plt.xlabel('Data')
+plt.legend()
+st.pyplot(plt)
+
+st.write("""## Previsão""")
+
+#results_AR.plot_predict(1,100) 
+
+mes = st.selectbox('Escolha o mês:', [i for i in range(1,13)])
+
+st.write(results_AR.forecast(steps=mes).values[-1])
+
+
